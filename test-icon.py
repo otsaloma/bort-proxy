fa8e17ee90a872de6c4983aa23d7d8517fcacffd
Osmo Salomaa <otsaloma@iki.fi>
2018-01-27 21:12:34 +0200

    Test URLs from command line

--- test-icon.py
+++ test-icon.py
@@ -2,10 +2,11 @@
 
 import importlib
 import os
+import sys
 
 path = os.path.join(os.path.dirname(__file__), "app.py")
 loader = importlib.machinery.SourceFileLoader("app", path)
 app = loader.load_module("app")
 
-for icon in app.find_icons("github.com"):
+for icon in app.find_icons(sys.argv[1]):
     print(icon)
