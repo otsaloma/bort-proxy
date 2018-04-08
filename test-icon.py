cce55515058251a02740392c9125c6dbdcbc4d48
Osmo Salomaa <otsaloma@iki.fi>
2018-04-08 05:11:18 +0300

    Simplify

--- test-icon.py
+++ test-icon.py
@@ -1,12 +1,7 @@
 #!/usr/bin/env python3
 
-import importlib
-import os
+import app
 import sys
-
-path = os.path.join(os.path.dirname(__file__), "app.py")
-loader = importlib.machinery.SourceFileLoader("app", path)
-app = loader.load_module("app")
 
 for icon in app.find_icons(sys.argv[1]):
     print(icon)
